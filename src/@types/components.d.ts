3366373b2c4919daff07c50512e6d83ad1b2a426
liuruijun <ruijun.liu-a1654@aqara.com>
2022-04-11 20:06:04 +0800

    feat: 同上

--- src/@types/components.d.ts
+++ src/@types/components.d.ts
@@ -9,7 +9,6 @@ declare module 'vue' {
         ElBreadcrumbItem: typeof import('element-plus/es')['ElBreadcrumbItem'];
         ElButton: typeof import('element-plus/es')['ElButton'];
         ElConfigProvider: typeof import('element-plus/es')['ElConfigProvider'];
-        ElDatePicker: typeof import('element-plus/es')['ElDatePicker'];
         ElDropdown: typeof import('element-plus/es')['ElDropdown'];
         ElDropdownItem: typeof import('element-plus/es')['ElDropdownItem'];
         ElDropdownMenu: typeof import('element-plus/es')['ElDropdownMenu'];
